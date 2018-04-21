6ff797de9e86a47caf81cb4973eff7eceb9e43bf
mhjensen <morten.hjorth-jensen@fys.uio.no>
2018-04-21 13:21:06 -0400

    added sgd method

--- doc/Programs/VMC/qdoteminim.py
+++ doc/Programs/VMC/qdoteminim.py
@@ -12,12 +12,6 @@ import sys
 from numba import jit
 
 
-#Read name of output file from command line
-if len(sys.argv) == 2:
-    outfilename = sys.argv[1]
-else:
-    print('\nError: Name of output file must be given as command line argument.\n')
-outfile = open(outfilename,'w')
 
 # Trial wave function for the 2-electron quantum dot in two dims
 def WaveFunction(r,alpha,beta):
@@ -46,8 +40,8 @@ def DerivativeWFansatz(r,alpha,beta):
     r12 = sqrt((r[0,0]-r[1,0])**2 + (r[0,1]-r[1,1])**2)
     deno = 1.0/(1+beta*r12)
     deno2 = deno*deno
-    WfDer(0) = -(r1+r2)
-    WfDer(1) = -r12*deno2
+    WfDer[0] = -(r1+r2)
+    WfDer[1] = -r12*deno2
     return  WfDer
 
 # Setting up the quantum force for the two-electron quantum dot, recall that it is a vector
@@ -60,13 +54,13 @@ def QuantumForce(r,alpha,beta):
     qforce[1,:] = -2*r[1,:]*alpha*(r[1,:]-r[0,:])*deno*deno/r12
     return qforce
     
-# The Monte Carlo sampling with the Metropolis algo
+# Computing the derivative of the energy and the energy 
 # jit decorator tells Numba to compile this function.
 # The argument types will be inferred by Numba when function is called.
 @jit
-def EnergyMinimization():
+def EnergyMinimization(alpha, beta):
 
-    NumberMCcycles= 100000
+    NumberMCcycles= 10000
     # Parameters in the Fokker-Planck simulation of the quantum force
     D = 0.5
     TimeStep = 0.05
@@ -79,77 +73,62 @@ def EnergyMinimization():
 
     # seed for rng generator 
     seed()
-    # start variational parameter  loops, two parameters here
+    energy = 0.0
+    DeltaE = 0.0
+    EnergyDer = np.zeros((2), np.double)
+    DeltaPsi = np.zeros((2), np.double)
+    DerivativePsiE = np.zeros((2), np.double)
+    #Initial position
+    for i in range(NumberParticles):
+        for j in range(Dimension):
+            PositionOld[i,j] = normalvariate(0.0,1.0)*sqrt(TimeStep)
+    wfold = WaveFunction(PositionOld,alpha,beta)
+    QuantumForceOld = QuantumForce(PositionOld,alpha, beta)
 
-            energy = 0.0
-            DeltaE = 0.0
-            DeltaPsi = np.zeros((2), np.double)
-            DerivativeE = np.zeros((2), np.double)
-            #Initial position
-            for i in range(NumberParticles):
-                for j in range(Dimension):
-                    PositionOld[i,j] = normalvariate(0.0,1.0)*sqrt(TimeStep)
-            wfold = WaveFunction(PositionOld,alpha,beta)
-            QuantumForceOld = QuantumForce(PositionOld,alpha, beta)
-
-            #Loop over MC MCcycles
-            for MCcycle in range(NumberMCcycles):
-                #Trial position moving one particle at the time
-                for i in range(NumberParticles):
-                    for j in range(Dimension):
-                        PositionNew[i,j] = PositionOld[i,j]+normalvariate(0.0,1.0)*sqrt(TimeStep)+\
-                                           QuantumForceOld[i,j]*TimeStep*D
-                    wfnew = WaveFunction(PositionNew,alpha,beta)
-                    QuantumForceNew = QuantumForce(PositionNew,alpha, beta)
-                    GreensFunction = 0.0
-                    for j in range(Dimension):
-                        GreensFunction += 0.5*(QuantumForceOld[i,j]+QuantumForceNew[i,j])*\
+    #Loop over MC MCcycles
+    for MCcycle in range(NumberMCcycles):
+        #Trial position moving one particle at the time
+        for i in range(NumberParticles):
+            for j in range(Dimension):
+                PositionNew[i,j] = PositionOld[i,j]+normalvariate(0.0,1.0)*sqrt(TimeStep)+\
+                                       QuantumForceOld[i,j]*TimeStep*D
+            wfnew = WaveFunction(PositionNew,alpha,beta)
+            QuantumForceNew = QuantumForce(PositionNew,alpha, beta)
+            GreensFunction = 0.0
+            for j in range(Dimension):
+                GreensFunction += 0.5*(QuantumForceOld[i,j]+QuantumForceNew[i,j])*\
 	                              (D*TimeStep*0.5*(QuantumForceOld[i,j]-QuantumForceNew[i,j])-\
                                       PositionNew[i,j]+PositionOld[i,j])
       
-                    GreensFunction = exp(GreensFunction)
-                    ProbabilityRatio = GreensFunction*wfnew**2/wfold**2
-                    #Metropolis-Hastings test to see whether we accept the move
-                    if random() <= ProbabilityRatio:
-                       for j in range(Dimension):
-                           PositionOld[i,j] = PositionNew[i,j]
-                           QuantumForceOld[i,j] = QuantumForceNew[i,j]
-                       wfold = wfnew
-                DeltaE = LocalEnergy(PositionOld,alpha,beta)
-                DeltaPsi = DerivativeWFansatz(r,alpha,beta):
-                energy += DeltaE
-                DerivativePsiE += DeltaPsi*DeltaE
+            GreensFunction = exp(GreensFunction)
+            ProbabilityRatio = GreensFunction*wfnew**2/wfold**2
+            #Metropolis-Hastings test to see whether we accept the move
+            if random() <= ProbabilityRatio:
+                for j in range(Dimension):
+                    PositionOld[i,j] = PositionNew[i,j]
+                    QuantumForceOld[i,j] = QuantumForceNew[i,j]
+                wfold = wfnew
+        DeltaE = LocalEnergy(PositionOld,alpha,beta)
+        DeltaPsi = DerivativeWFansatz(PositionOld,alpha,beta)
+        energy += DeltaE
+        DerivativePsiE += DeltaPsi*DeltaE
             
-            # We calculate mean, variance and error (no blocking applied)
-            energy /= NumberMCcycles
+    # We calculate mean, variance and error (no blocking applied)
+    energy /= NumberMCcycles
+    DerivativePsiE /= NumberMCcycles
+    DeltaPsi /= NumberMCcycles
+    EnergyDer  = 2*(DerivativePsiE-DeltaPsi*energy)
     return energy, EnergyDer
 
 
 #Here starts the main program with variable declarations
 NumberParticles = 2
 Dimension = 2
-MaxVariations = 10
-Energies = np.zeros((MaxVariations,MaxVariations))
-AlphaValues = np.zeros(MaxVariations)
-BetaValues = np.zeros(MaxVariations)
-(Energies, AlphaValues, BetaValues) = MonteCarloSampling()
-outfile.close()
-# Prepare for plots
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-# Plot the surface.
-X, Y = np.meshgrid(AlphaValues, BetaValues)
-surf = ax.plot_surface(X, Y, Energies,cmap=cm.coolwarm,linewidth=0, antialiased=False)
-# Customize the z axis.
-zmin = np.matrix(Energies).min()
-zmax = np.matrix(Energies).max()
-ax.set_zlim(zmin, zmax)
-ax.set_xlabel(r'$\alpha$')
-ax.set_ylabel(r'$\beta$')
-ax.set_zlabel(r'$\langle E \rangle$')
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.show()
-
+# guess for variational parameters
+alpha = 0.9
+beta = 0.3
+# Set up iteration using stochastic gradient method
+Energy = 0
+EDerivative = np.zeros((2), np.double)
+Energy, EDerivative = EnergyMinimization(alpha,beta)
+print(Energy, EDerivative[0], EDerivative[1])
